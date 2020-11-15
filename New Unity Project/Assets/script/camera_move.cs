e83ef64cba3e6695b1140a0ee4c38c965fd68693
csp7645 <74478346+csp7645@users.noreply.github.com>
2020-11-15 14:19:22 +0900

    test
    sss

---
+++ New Unity Project/Assets/script/camera_move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour
+{
+
+    Vector3 camera_interval;
+    public GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        /*공과 카메라간격*/
+        camera_interval = transform.position - obj.transform.position;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        /*카메라랑 동시에 구체 움직임*/
+        transform.position =  obj.transform.position + camera_interval;
+
+    }
+}
---
+++ New Unity Project/Assets/script/move.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    int IsDoubleJump = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float Time_interval = Time.deltaTime;
+        float speed = 10;
+
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && 0 < IsDoubleJump)
+        {
+            //transform.position += Time_interval* Vector3.up * speed;
+
+            IsDoubleJump -= 1;
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
+            
+            
+        }
+
+        /*if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            transform.position += Time_interval * Vector3.down * speed;
+        }*/
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            transform.position += Time_interval * Vector3.right * speed;
+
+        }
+      
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Time_interval * Vector3.left * speed;
+        }
+
+
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsDoubleJump = 3;
+    }
+
+}
